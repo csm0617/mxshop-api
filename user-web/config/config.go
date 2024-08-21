26d315c43edf304358f7c6530cd7fa2cac9a7902
‘陈水明’ <439752552@qq.com>
2024-08-21 18:33:04 +0800

    feat(config): 新增jwt的key配置

--- user-web/config/config.go
+++ user-web/config/config.go
@@ -1,12 +1,26 @@
 package config
 
+/*
+user-grpc服务配置
+*/
 type UserSrvConfig struct {
 	Host string `mapstructure:"host"`
 	Port int    `mapstructure:"port"`
 }
 
+/*
+jwt配置
+*/
+type JWTConfig struct {
+	SigningKey string `mapstructure:"key"`
+}
+
+/*
+当前user-web服务配置
+*/
 type ServerConfig struct {
 	Name        string        `mapstructure:"name"`
 	Port        int           `mapstructure:"port"`
 	UserSrvInfo UserSrvConfig `mapstructure:"user-srv"`
+	JWTInfo     JWTConfig     `mapstructure:"jwt"`
 }
