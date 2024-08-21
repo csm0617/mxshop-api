3da8df2c1abbf7b761269838f1aaeeb8ea8b3335
‘陈水明’ <439752552@qq.com>
2024-08-22 07:41:55 +0800

    feat(forms): 登录表单新增图形验证码和图形验证码id字段

--- user-web/forms/user.go
+++ user-web/forms/user.go
@@ -1,6 +1,8 @@
 package forms
 
 type PassWordLoginForm struct {
-	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobileValidation"`
-	PassWord string `form:"password" json:"password" binding:"required,min=3,max=20"`
+	Mobile    string `form:"mobile" json:"mobile" binding:"required,mobileValidation"`
+	PassWord  string `form:"password" json:"password" binding:"required,min=3,max=20"`
+	Captcha   string `form:"captcha" json:"captcha" binding:"required,min=4,max=4"`
+	CaptchaId string `form:"captcha_id" json:"captcha_id" binding:"required"`
 }
