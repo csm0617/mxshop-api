e0a60deea383822167baa59c717195e50e00c27e
‘陈水明’ <439752552@qq.com>
2024-08-21 18:55:13 +0800

    feat(router): 给查询用户的接口加上jwt校验

--- user-web/router/user.go
+++ user-web/router/user.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"mxshop_api/user-web/api"
+	"mxshop_api/user-web/middlewares"
 )
 
 func InitUserRouter(router *gin.RouterGroup) {
@@ -11,8 +12,8 @@ func InitUserRouter(router *gin.RouterGroup) {
 	UserRouter := router.Group("user")
 	//router下的api
 	{
-
-		UserRouter.GET("list", api.GetUserList)
+		//对访问用户列表加上登录的jwt验证
+		UserRouter.GET("list", middlewares.JWTAuth(), api.GetUserList)
 		UserRouter.POST("login", api.PassWordLoginForm)
 
 	}
