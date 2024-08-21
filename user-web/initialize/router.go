a264c22b16c92592060b89e2817c1b79cfc5a7ae
‘陈水明’ <439752552@qq.com>
2024-08-21 20:08:12 +0800

    feat(initialize): 新全局请求新增跨域处理

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -3,12 +3,14 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 
+	"mxshop_api/user-web/middlewares"
 	userRouter "mxshop_api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 
 	Router := gin.Default()
+	Router.Use(middlewares.Cors()) //对全局的请求添加跨域处理的中间件
 	//定义全局的路由分组（同时也是定义api版本）
 	ApiGroup := Router.Group("v1")
 	userRouter.InitUserRouter(ApiGroup)
