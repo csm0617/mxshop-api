796f3c90ca302725e5e96089ff36af78f5d972a7
‘陈水明’ <439752552@qq.com>
2024-08-24 15:20:05 +0800

    feat(global): 新增UserSrvClient全局变量

--- user-web/global/global.go
+++ user-web/global/global.go
@@ -4,10 +4,12 @@ import (
 	ut "github.com/go-playground/universal-translator"
 
 	"mxshop_api/user-web/config"
+	"mxshop_api/user-web/proto"
 )
 
 // 啥也不干，专门用来定义全局变量
 var (
-	Trans        ut.Translator
-	ServerConfig *config.ServerConfig = &config.ServerConfig{} //因为经常要变化，所以采用指针并且初始化
+	Trans         ut.Translator                                 //翻译器
+	ServerConfig  *config.ServerConfig = &config.ServerConfig{} //因为经常要变化，所以采用指针并且初始化
+	UserSrvClient proto.UserClient                              //用户服务grpc客户端
 )
